ae1edeef613a3b06a7162fe4ecc7d7f227883a00
Caro-L-dev <liardcar@gmail.com>
2024-06-13 19:19:09 +0200

    feat: init repository

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center m-auto max-w-4xl lg:flex-col gap-8 min-h-full">
+      <div>CARD</div>
+      <div>PREVIEW</div>
+    </main>
+  );
+}
